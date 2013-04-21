142faf471dece5644393a1781fb8fec51ef6a59d
kongo2002 <kongo2002@googlemail.com>
2013-04-22 00:06:11 +0200

    add debug print macro(s)

--- src/webserver.c
+++ src/webserver.c
@@ -3,9 +3,11 @@
 #pragma comment(lib, "psapi.lib")
 #pragma comment(lib, "Iphlpapi.lib")
 
+#include <assert.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
+
 #include "uv.h"
 #include "http_parser.h"
 
@@ -16,6 +18,25 @@
     UVERR(err, msg); \
     exit(1); \
   }
+
+#ifndef DEBUG
+# ifdef NDEBUG
+#  define DEBUG 0
+# else
+#  define DEBUG 1
+# endif
+#endif
+
+void dbg_print(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+}
+
+#define DPUTS(msg) do { if (DEBUG) fputs(msg, stderr); } while (0)
+#define DPRINT(x) do { if (DEBUG) dbg_print x; } while (0)
 
 //"Connection: Keep-Alive\r\n" \
 
@@ -34,7 +55,7 @@ static http_parser_settings parser_settings;
 
 static uv_buf_t resbuf;
 
-typedef struct 
+typedef struct
 {
     uv_tcp_t handle;
     http_parser parser;
@@ -42,17 +63,17 @@ typedef struct
     int request_num;
 } client_t;
 
-//static int connection_number = 0;
-//static int request_number = 0;
+static int connection_number = 0;
+static int request_number = 0;
 
 void on_close(uv_handle_t* handle)
 {
-    //printf("    on_close()\n");
+    DPUTS(("    on_close()\n"));
     client_t* client = (client_t*) handle->data;
     free(client);
 }
 
-uv_buf_t on_alloc(uv_handle_t* client, size_t suggested_size) 
+uv_buf_t on_alloc(uv_handle_t* client, size_t suggested_size)
 {
     uv_buf_t buf;
     buf.base = (char *)malloc(suggested_size * 10);
@@ -60,26 +81,26 @@ uv_buf_t on_alloc(uv_handle_t* client, size_t suggested_size)
     return buf;
 }
 
-void on_read(uv_stream_t* tcp, ssize_t nread, uv_buf_t buf) 
+void on_read(uv_stream_t* tcp, ssize_t nread, uv_buf_t buf)
 {
-    //printf("    on_read()\n");
+    DPUTS(("    on_read()\n"));
     size_t parsed;
     client_t* client = (client_t*) tcp->data;
 
-    if (nread >= 0) 
+    if (nread >= 0)
     {
         parsed = http_parser_execute(&client->parser, &parser_settings, buf.base, nread);
-        if (parsed < nread) 
+        if (parsed < nread)
         {
-            //printf("    ERROR on_read() parsed < nread\n");
+            DPUTS(("    ERROR on_read() parsed < nread\n"));
             //uv_close((uv_handle_t*) &client->handle, on_close);
         }
-    } 
-    else 
+    }
+    else
     {
-        //printf("    ERROR on_read() nread < 0\n");
+        DPUTS(("    ERROR on_read() nread < 0\n"));
         uv_err_t err = uv_last_error(uv_loop);
-        if (err.code != UV_EOF) 
+        if (err.code != UV_EOF)
         {
             UVERR(err, "read");
         }
@@ -90,10 +111,10 @@ void on_read(uv_stream_t* tcp, ssize_t nread, uv_buf_t buf)
 
 static int request_num = 1;
 
-void on_connect(uv_stream_t* server_handle, int status) 
+void on_connect(uv_stream_t* server_handle, int status)
 {
-    //printf("BEGIN CONNECTION #%d\n", connection_number);
-    //printf("    on_connect() status:%d\n", status);
+    DPRINT(("BEGIN CONNECTION #%d\n", connection_number));
+    DPRINT(("    on_connect() status:%d\n", status));
     int r;
 
     client_t* client = (client_t *)malloc(sizeof(client_t));
@@ -106,27 +127,27 @@ void on_connect(uv_stream_t* server_handle, int status)
     client->handle.data = client;
 
     r = uv_accept(server_handle, (uv_stream_t*)&client->handle);
-    
-    //printf("    uv_accept() returned:%d\n", r);
+
+    DPRINT(("    uv_accept() returned:%d\n", r));
 
     r = uv_read_start((uv_stream_t*)&client->handle, on_alloc, on_read);
-    //printf("    uv_read_start() returned:%d\n", r);
+    DPRINT(("    uv_read_start() returned:%d\n", r));
 }
 
-void after_write(uv_write_t* req, int status) 
+void after_write(uv_write_t* req, int status)
 {
     //uv_close((uv_handle_t*)req->handle, on_close);
-    //printf("after_write() status:%d\n", status);
+    DPRINT(("after_write() status:%d\n", status));
     free(req);
 }
 
-int write_response(http_parser* parser) 
+int write_response(http_parser* parser)
 {
     client_t* client = (client_t*) parser->data;
-    
-    //printf("    write_response()\n");
+
+    DPUTS(("    write_response()\n"));
     uv_write_t* write_req = malloc(sizeof(*write_req));
-  
+
     int r = uv_write(
         //&client->write_req,
         write_req,
@@ -135,30 +156,30 @@ int write_response(http_parser* parser)
         1,
         after_write);
 
-    //printf("    uv_write() returned:%d\n", r);
+    DPRINT(("    uv_write() returned:%d\n", r));
 
     return 0;
 }
 
-int on_headers_complete(http_parser* parser) 
+int on_headers_complete(http_parser* parser)
 {
-    //printf("    on_headers_complete()\n");
+    DPUTS(("    on_headers_complete()\n"));
     write_response(parser);
-    
+
     return 0;
 }
 
 int on_message_begin(http_parser* parser)
 {
-    //printf("BEGIN REQUEST #%d\n", request_number);
-    //printf("    on_message_begin()\n");
+    DPRINT(("BEGIN REQUEST #%d\n", request_number));
+    DPUTS(("    on_message_begin()\n"));
     return 0;
 }
 
 int on_message_complete(http_parser* parser)
 {
-    //printf("    on_message_complete()\n");
-    //printf("END REQUEST #%d\n", request_number);
+    DPUTS(("    on_message_complete()\n"));
+    DPRINT(("END REQUEST #%d\n", request_number));
     return 0;
 }
 
@@ -169,7 +190,7 @@ int start_server()
     parser_settings.on_headers_complete = on_headers_complete;
     parser_settings.on_message_begin = on_message_begin;
     parser_settings.on_message_complete = on_message_complete;
-  
+
     resbuf.base = RESPONSE;
     resbuf.len = sizeof(RESPONSE);
 

ebc821c17da0d1f293437e4cc22face4cd29116c
Joseph Crail <jbcrail@gmail.com>
2013-05-21 20:43:39 -0400

    Fix implicit declaration warnings.

--- src/haywire/http_request.c
+++ src/haywire/http_request.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "haywire.h"
 #include "http_request.h"
 #include "http_parser.h"
--- src/haywire/trie/route_compare_method.c
+++ src/haywire/trie/route_compare_method.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include "route_compare_method.h"
 #include "radix.h"
 

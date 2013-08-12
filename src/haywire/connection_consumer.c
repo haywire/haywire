d86e11fdddb6372c176c58d2d32de1c2d385829b
Kelly Sommers <kell.sommers@gmail.com>
2013-08-11 21:44:55 -0400

    Adding missing files from event-loop dispatching check in.

---
+++ src/haywire/connection_consumer.c
@@ -0,0 +1,109 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "uv.h"
+#include "connection_consumer.h"
+#include "http_server.h"
+#include "http_connection.h"
+
+void ipc_read2_cb(uv_pipe_t* ipc_pipe, ssize_t nread, uv_buf_t buf, uv_handle_type type)
+{
+    int rc;
+    struct ipc_client_ctx* ctx;
+    uv_loop_t* loop;
+    
+    ctx = container_of(ipc_pipe, struct ipc_client_ctx, ipc_pipe);
+    loop = ipc_pipe->loop;
+    
+    if (type == UV_TCP)
+        rc = uv_tcp_init(loop, (uv_tcp_t*) ctx->server_handle);
+    else if (type == UV_NAMED_PIPE)
+        rc = uv_pipe_init(loop, (uv_pipe_t*) ctx->server_handle, 0);
+    
+    rc = uv_accept((uv_stream_t*) &ctx->ipc_pipe, ctx->server_handle);
+    uv_close((uv_handle_t*) &ctx->ipc_pipe, NULL);
+}
+
+uv_buf_t ipc_alloc_cb(uv_handle_t* handle, size_t suggested_size)
+{
+    struct ipc_client_ctx* ctx;
+    ctx = container_of(handle, struct ipc_client_ctx, ipc_pipe);
+    return uv_buf_init(ctx->scratch, sizeof(ctx->scratch));
+}
+
+void ipc_connect_cb(uv_connect_t* req, int status)
+{
+    int rc;
+    struct ipc_client_ctx* ctx;
+    ctx = container_of(req, struct ipc_client_ctx, connect_req);
+    rc = uv_read2_start((uv_stream_t*)&ctx->ipc_pipe, ipc_alloc_cb, ipc_read2_cb);
+}
+
+void sv_read_cb(uv_stream_t* handle, ssize_t nread, uv_buf_t buf)
+{
+    //ASSERT(nread == UV_EOF);
+    uv_close((uv_handle_t*) handle, (uv_close_cb)free);
+}
+
+uv_buf_t connection_consumer_alloc(uv_handle_t* handle, size_t suggested_size)
+{
+    static char buf[32];
+    return uv_buf_init(buf, sizeof(buf));
+}
+
+void connection_consumer_new_connection(uv_stream_t* server_handle, int status)
+{
+    http_connection* connection = create_http_connection();
+    http_parser_init(&connection->parser, HTTP_REQUEST);
+    
+    connection->parser.data = connection;
+    connection->stream.data = connection;
+    
+    int rc = 0;
+    rc = uv_tcp_init(server_handle->loop, &connection->stream);
+    rc = uv_accept(server_handle, (uv_stream_t*)&connection->stream);
+    rc = uv_read_start((uv_stream_t*)&connection->stream, http_stream_on_alloc, http_stream_on_read);
+}
+
+void connection_consumer_close(uv_async_t* handle, int status)
+{
+    struct server_ctx* ctx;
+    ctx = container_of(handle, struct server_ctx, async_handle);
+    uv_close((uv_handle_t*) &ctx->server_handle, NULL);
+    uv_close((uv_handle_t*) &ctx->async_handle, NULL);
+}
+
+void get_listen_handle(uv_loop_t* loop, uv_stream_t* server_handle)
+{
+    int rc;
+    struct ipc_client_ctx ctx;
+    
+    ctx.server_handle = server_handle;
+    ctx.server_handle->data = "server handle";
+    
+    rc = uv_pipe_init(loop, &ctx.ipc_pipe, 1);
+    uv_pipe_connect(&ctx.connect_req, &ctx.ipc_pipe, "HAYWIRE_CONNECTION_DISPATCH_PIPE_NAME", ipc_connect_cb);
+    rc = uv_run(loop, UV_RUN_DEFAULT);
+}
+
+void connection_consumer_start(void *arg)
+{
+    int rc;
+    struct server_ctx *ctx;
+    uv_loop_t* loop;
+    
+    ctx = arg;
+    loop = uv_loop_new();
+    
+    rc = uv_async_init(loop, &ctx->async_handle, connection_consumer_close);
+    uv_unref((uv_handle_t*) &ctx->async_handle);
+    
+    /* Wait until the main thread is ready. */
+    uv_sem_wait(&ctx->semaphore);
+    get_listen_handle(loop, (uv_stream_t*) &ctx->server_handle);
+    uv_sem_post(&ctx->semaphore);
+    
+    rc = uv_listen((uv_stream_t*)&ctx->server_handle, 128, connection_consumer_new_connection);
+    rc = uv_run(loop, UV_RUN_DEFAULT);
+    
+    uv_loop_delete(loop);
+}
---
+++ src/haywire/connection_consumer.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "uv.h"
+
+#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+
+#define container_of(ptr, type, member) ({            \
+const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
+(type *)( (char *)__mptr - offsetof(type,member) );})
+
+union stream_handle2
+{
+    uv_pipe_t pipe;
+    uv_tcp_t tcp;
+};
+
+typedef unsigned char handle_storage_t[sizeof(union stream_handle2)];
+
+struct server_ctx
+{
+    handle_storage_t server_handle;
+    unsigned int num_connects;
+    uv_async_t async_handle;
+    uv_thread_t thread_id;
+    uv_sem_t semaphore;
+};
+
+struct ipc_client_ctx
+{
+    uv_connect_t connect_req;
+    uv_stream_t* server_handle;
+    uv_pipe_t ipc_pipe;
+    char scratch[16];
+};
+
+struct ipc_server_ctx
+{
+    handle_storage_t server_handle;
+    unsigned int num_connects;
+    uv_pipe_t ipc_pipe;
+};
+
+void connection_consumer_start(void *arg);

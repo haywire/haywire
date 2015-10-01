a07b3ffde574e8dd8734229c56b22a8ff84e80da
Kelly Sommers <kell.sommers@gmail.com>
2015-10-01 15:04:00 -0400

    Update http_connection.h

--- src/haywire/http_connection.h
+++ src/haywire/http_connection.h
@@ -1,23 +1,2 @@
 #pragma once
-#include "uv.h"
-#include "http_parser.h"
-#include "http_request.h"
-
-typedef struct
-{
-    uv_tcp_t stream;
-    http_parser parser;
-    uv_write_t write_req;
-    http_request* request;
-    char current_header_key[1024];
-    int current_header_key_length;
-    char current_header_value[1024];
-    int current_header_value_length;
-    int keep_alive;
-    int last_was_value;
-    uv_buf_t response_buffers[1024];
-    int response_buffers_count;
-    int prevbuflen;
-    char request_buffer[4000096];
-    int request_buffer_length;
-} http_connection;
+#include "http.h"

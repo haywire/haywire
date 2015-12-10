db2bc70579ad60d172b7d1acc8a7dee2613289ac
Kelly Sommers <kell.sommers@gmail.com>
2015-12-10 13:25:39 -0500

    Changing connection structure to store url as a hw_string for better control.

--- include/haywire.h
+++ include/haywire.h
@@ -156,7 +156,7 @@ typedef struct
     unsigned short http_minor;
     unsigned char method;
     int keep_alive;
-	char* url;
+    hw_string* url;
     void* headers;
     hw_string* body;
     int body_length;
